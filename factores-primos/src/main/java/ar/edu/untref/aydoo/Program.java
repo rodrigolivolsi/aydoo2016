12bbc2a5df22f676be650f0db03e77a8aec91bb7
rodrigolivolsi <livolsi.rod@gmail.com>
2016-03-23 03:41:55 -0700

    Ejercicio Factores-primos completo

--- factores-primos/src/main/java/ar/edu/untref/aydoo/Program.java
+++ factores-primos/src/main/java/ar/edu/untref/aydoo/Program.java
@@ -2,8 +2,33 @@ package ar.edu.untref.aydoo;
 
 public class Program 
 {
-    public static final void main(String arg[])
+   
+	public static final void descomponerNumero(int numero)
     {
-        System.out.println("hola!");
-    }
+        
+
+		System.out.print("\nLos factores primos del numero ingresado son " + numero + ": ");
+
+		
+		int numeroNuevo = numero;
+		int n = 2;
+
+		while(numeroNuevo >= 2){
+
+			if(numeroNuevo % n == 0){
+
+				numeroNuevo = numeroNuevo / n;
+				System.out.print(n + " ");
+
+			}else{
+
+				n++;
+
+			}
+
+		}   
+
+	}
+
+
 }
